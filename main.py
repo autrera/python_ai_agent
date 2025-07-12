6885a071f3173f7c1db83339f075a101c2318fb0
Aldo Utrera <accounts@aldoutrera.com>
2025-07-12 08:26:50 -0500

    Accept a prompt from the command line instead of hardcoding it

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
+
+parameters = sys.argv[1:]
+prompt = parameters[0] if len(parameters) > 0 else ""
 
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
 def main():
+    if prompt == "":
+        print("A prompt needs to be provided!")
+        exit(1)
+
     print("Hello from python-ai-agent!")
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum please'
+        model='gemini-2.0-flash-001', contents=prompt
     )
     print(response.text)
     print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
